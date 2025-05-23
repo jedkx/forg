1d5fa50b651d2255ab28127891e8759865e9f78e
jedkx <ccoskun742@gmail.com>
2025-05-23 12:04:04 +0300

    collison rotasyon sorunu cozuldu

--- Scene/SwordHitboxComponent.gd
+++ Scene/SwordHitboxComponent.gd
@@ -1,9 +1,22 @@
 extends Area2D
 
+@onready var parent_sprite = get_parent().get_node("Sprite2D")
 @export var damage: float = 20.0
+
+var last_flip_state: bool = false
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
+	last_flip_state = parent_sprite.flip_h
+	update_scale()
+	
+func _process(_delta):
+	if parent_sprite.flip_h != last_flip_state:
+		last_flip_state = parent_sprite.flip_h
+		update_scale()
+
+func update_scale():
+	scale.x = -1 if parent_sprite.flip_h else 1
 
 func _on_body_entered(body):
 	if body.is_in_group("enemy") and body.has_node("EnemyHealth"):
